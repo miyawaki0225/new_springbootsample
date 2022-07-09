8afc4142f3be6a48645676f2414473580658d2b9
m-miyawaki <m-miyawaki@rapide-act.com>
2022-07-10 00:27:42 +0900

    初出:decorate

---
+++ memo_/chapter07.md
@@ -0,0 +1,3 @@
+
+- xmlns:layoutとlayout:replaceの違い
+https://qiita.com/rubytomato@github/items/cbfe7540da0e2e35318a
--- src/main/java/com/example/springbootsample/controller/LoginController.java
+++ src/main/java/com/example/springbootsample/controller/LoginController.java
@@ -2,11 +2,17 @@ package com.example.springbootsample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String getLogin() {
+    public String getLogin(){
         return "login/login";
     }
+    /* ユーザー一覧画面にリダイレクト */
+    @PostMapping("/login")
+    public String postLogin(){
+        return "redirect:/user/list";
+    }    
 }
