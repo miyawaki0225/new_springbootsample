9f2efe82ea47a503d8dd94e1d9854c45cd1aa778
m-miyawaki <m-miyawaki@rapide-act.com>
2022-07-10 15:08:13 +0900

    初出：Param

--- src/main/java/com/example/springbootsample/repository/UserMapper.java
+++ src/main/java/com/example/springbootsample/repository/UserMapper.java
@@ -3,12 +3,20 @@ package com.example.springbootsample.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.springbootsample.domain.user.model.MUser;
 
 @Mapper
 public interface UserMapper {
+    /* ユーザー登録 */
     public int insertOne(MUser user);
-
+    /* ユーザー取得 */
     public List<MUser>findMany();
+    /* ユーザー取得（１件） */
+    public MUser findOne(String userId);
+
+    /*@Param　複数のパラメータを使用する場合、メソッドの引数に@Paramアノテーションをつける */
+    /* ユーザー削除（１件） */
+    public int deleteOne(@Param("userId") String userId);
 }
