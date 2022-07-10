2832a25cf45a63d11c41037aea0f2302ed4466e1
m-miyawaki <m-miyawaki@rapide-act.com>
2022-07-11 00:25:02 +0900

    エラー発生中：bean type がないといわれてる

--- src/main/java/com/example/springbootsample/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootsample/domain/user/service/impl/UserServiceImpl.java
@@ -30,9 +30,6 @@ public class UserServiceImpl implements UserService{
 		user.setPassword(encoder.encode(rawPassword));
 		
 		mapper.insertOne(user);
-
-
-        mapper.insertOne(user);
     }
 
     /* ユーザー取得 */
@@ -54,7 +51,10 @@ public class UserServiceImpl implements UserService{
 	public void updateUserOne(String userId,
 			String password,
 			String userName) {
-		mapper.updateOne(userId, password, userName);
+
+        String encryptPassword = encoder.encode(password);
+		
+        mapper.updateOne(userId, encryptPassword, userName);
         // 例外を発生させる。
         // int i= 1/0;
 	}
